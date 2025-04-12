98d80da533a7142e65b9177363da87040490d508
Erkanat <yerkanat.kulman@narxoz.kz>
2025-04-12 15:29:45 +0500

    feat: migration

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"github.com/erkanattt/go-rest-crud-project/internal/models"
+	"github.com/erkanattt/go-rest-crud-project/internal/db"
 	"github.com/erkanattt/go-rest-crud-project/internal/routes"
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"log"
 )
 
 func main() {
-	// Postgres контейнерінің атауын қолдану
-	db, err := gorm.Open(postgres.Open("postgres://postgres:7982@postgres:5432/postgres?sslmode=disable"), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Error connecting to the database:", err)
-	}
-
-	err = db.AutoMigrate(&models.Product{})
-	if err != nil {
-		log.Fatal("Error on migrating to the DB", err)
-	}
+	db.InitDB()
 
 	r := gin.Default()
-	routes.SetupRoutes(r, db)
+	routes.SetupRoutes(r, db.DB)
 	r.Run(":8080")
 }
