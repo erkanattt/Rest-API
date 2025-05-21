e5d323e33fd12fb099dde87a2df480ad6ba8a681
Erkanat <yerkanat.kulman@narxoz.kz>
2025-05-21 10:16:11 +0500

    feat: main

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,26 @@ func main() {
 	db.InitDB()
 
 	r := gin.Default()
+
+	r.Static("/static", "./frontend/static")
+
+	r.GET("/", func(c *gin.Context) {
+		c.File("../frontend/index.html")
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		c.File("../frontend/login.html")
+	})
+
+	r.GET("/register", func(c *gin.Context) {
+		c.File("../frontend/register.html")
+	})
+
+	r.GET("/admin", func(c *gin.Context) {
+		c.File("../frontend/admin.html")
+	})
+
 	routes.SetupRoutes(r, db.DB)
+
 	r.Run(":8080")
 }
